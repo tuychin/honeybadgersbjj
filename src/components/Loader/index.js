c17f0cac2d495d541e3a074e6258b0e39a8cec54
Ravil <tuychin@gmail.com>
2021-10-24 05:56:53 +0300

    feat(components): added Loader

---
+++ src/components/Loader/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {styled} from '@mui/material/styles';
+import CircularProgress from '@mui/material/CircularProgress';
+
+const Spinner = styled(CircularProgress)(() => ({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+}));
+
+const Loader = ({isActive = true}) => {
+  return isActive && <Spinner />;
+}
+
+Loader.propTypes = {
+  isActive: PropTypes.bool,
+};
+
+export default Loader;
--- src/components/YandexMap/index.js
+++ src/components/YandexMap/index.js
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {YMaps, Map, Placemark} from 'react-yandex-maps';
+import Box from '@mui/material/Box';
+
+import Loader from '../Loader';
 
 const YandexMap = ({coordinates}) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const mapChecker = () => {
+    const delay = 1000;
+
+    let timerId = setTimeout(function check () {
+      const yandexMapElem = document.querySelector('ymaps');
+
+      timerId = setTimeout(check, delay);
+
+      if (yandexMapElem) {
+        clearTimeout(timerId)
+        setIsLoaded(true)
+      }
+    }, delay);
+  }
+
+  useEffect(() => {
+    mapChecker();
+  }, [])
+
   return (
-    <YMaps>
-      <Map
-        width="100%"
-        defaultState={{center: coordinates, zoom: 15}}
-      >
-        <Placemark
-          modules={['geoObject.addon.balloon']}
-          geometry={coordinates}
-          properties={{
-            balloonContentBody: 'Honey badger BJJ team.<br>Улица Ленинская Слобода, 19',
-          }}
-          options={{
-            preset: 'islands#redSportIcon',
-          }}
-        />
-      </Map>
-    </YMaps>
+    <Box sx={{
+      position: 'relative',
+      minHeight: '240px',
+    }}>
+      <Loader isActive={!isLoaded} />
+      <YMaps>
+        <Map
+          width="100%"
+          defaultState={{center: coordinates, zoom: 15}}
+        >
+          <Placemark
+            modules={['geoObject.addon.balloon']}
+            geometry={coordinates}
+            properties={{
+              balloonContentBody: 'Honey badger BJJ team.<br>Улица Ленинская Слобода, 19',
+            }}
+            options={{
+              preset: 'islands#redSportIcon',
+            }}
+          />
+        </Map>
+      </YMaps>
+    </Box>
   );
 }
 
