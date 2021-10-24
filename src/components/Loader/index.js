255031538455ba79396cf913fd5b0068ffb22a27
Ravil <tuychin@gmail.com>
2021-10-24 05:59:20 +0300

    refactor(components): Loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -4,9 +4,6 @@ import {styled} from '@mui/material/styles';
 import CircularProgress from '@mui/material/CircularProgress';
 
 const Spinner = styled(CircularProgress)(() => ({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
   position: 'absolute',
   top: '50%',
   left: '50%',
