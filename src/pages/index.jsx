42c6b8099e036cd11b72828050dd02cf22ba1936
Ravil <tuychin@gmail.com>
2021-11-02 02:53:19 +0300

    feat(questions): added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import Showcase from '../sections/Showcase';
 import Benefits from '../sections/Benefits';
 import Coach from '../sections/Coach';
+import Questions from '../sections/Questions';
 
 const MainPage = () => (
   <main>
     <Showcase />
     <Benefits />
     <Coach />
+    <Questions />
   </main>
 );
 
---
+++ src/sections/Questions/index.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { styled } from '@mui/material/styles';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+import questions from './mock-data';
+
+const QuestionsWrapper = styled('section')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  paddingTop: theme.spacing(3),
+  paddingBottom: theme.spacing(3),
+  backgroundColor: theme.palette.background.default,
+}));
+
+const QuestionsInner = styled(Container)(() => ({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+}));
+
+const Questions = () => (
+  <QuestionsWrapper>
+    <Typography
+      sx={{
+        mb: {
+          xs: 5,
+          md: 8,
+        },
+      }}
+      variant="h2"
+      component="h2"
+    >
+      Вопросы
+    </Typography>
+    <QuestionsInner>
+      {questions.map(({ question, answer }) => (
+        <Accordion sx={{ width: '100%' }} key={question}>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography
+              variant="h6"
+              component="div"
+            >
+              {question}
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              {answer}
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </QuestionsInner>
+  </QuestionsWrapper>
+);
+
+export default Questions;
