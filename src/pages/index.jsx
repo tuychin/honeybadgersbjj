e97172e4f9003bc95e0f5d754614f70b345adaa8
Ravil <tuychin@gmail.com>
2021-11-02 02:15:57 +0300

    fix(Photos): delete

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,14 +2,12 @@ import * as React from 'react';
 import Showcase from '../sections/Showcase';
 import Benefits from '../sections/Benefits';
 import Coach from '../sections/Coach';
-import Photos from '../sections/Photos';
 
 const MainPage = () => (
   <main>
     <Showcase />
     <Benefits />
     <Coach />
-    <Photos />
   </main>
 );
 
