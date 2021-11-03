4f149be496be60e29cb87d457fa0436396f76272
Ravil <tuychin@gmail.com>
2021-11-04 01:42:17 +0300

    feat(components): added TargetBanner

---
+++ src/components/TargetBanner/index.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { styled } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+import { openContactModal } from '../ContactModal/contactModalSlice';
+import bjjBg from '../../images/bjj_bg.webp';
+
+const BannerWrapper = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  paddingTop: theme.spacing(12),
+  paddingBottom: theme.spacing(12),
+  backgroundImage: `url(${bjjBg})`,
+  backgroundColor: theme.palette.background.default,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  [theme.breakpoints.between('xs', 'md')]: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+}));
+
+const BannerInner = styled(Container)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  [theme.breakpoints.between('xs', 'md')]: {
+    flexDirection: 'column',
+  },
+}));
+
+const Banner = ({ text, buttonText }) => {
+  const dispatch = useDispatch();
+
+  const handleContactModalOpen = () => {
+    dispatch(openContactModal());
+  };
+
+  return (
+    <BannerWrapper>
+      <BannerInner>
+        <Typography
+          sx={{ mr: 3, mb: { xs: 4, md: 0 } }}
+          variant="h4"
+          component="span"
+        >
+          {text}
+        </Typography>
+        <Button
+          sx={{ flexShrink: 0, alignSelf: { xs: 'flex-start', md: 'center' } }}
+          size="large"
+          variant="contained"
+          color="primary"
+          onClick={handleContactModalOpen}
+        >
+          {buttonText}
+        </Button>
+      </BannerInner>
+    </BannerWrapper>
+  );
+};
+
+Banner.propTypes = {
+  text: PropTypes.string.isRequired,
+  buttonText: PropTypes.string,
+};
+
+export default Banner;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,7 @@ import Information from '../sections/Information';
 import Benefits from '../sections/Benefits';
 import Coach from '../sections/Coach';
 import Questions from '../sections/Questions';
+import TargetBanner from '../components/TargetBanner';
 
 const MainPage = () => (
   <main>
@@ -12,6 +13,10 @@ const MainPage = () => (
     <Benefits />
     <Coach />
     <Questions />
+    <TargetBanner
+      text="Что смотришь? Записывайся на первую тренировку, она бесплатная."
+      buttonText="Как записаться?"
+    />
   </main>
 );
 
