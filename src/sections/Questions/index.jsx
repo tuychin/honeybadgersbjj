5ba59efe621217d6e0af8d56a0774484164d1d55
Ravil <tuychin@gmail.com>
2021-11-15 01:42:57 +0300

    feat(Questions): get content from graphql

--- src/sections/Questions/index.jsx
+++ src/sections/Questions/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 import { styled } from '@mui/material/styles';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -7,7 +8,22 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
-import questionsMock from './mock-data';
+const useData = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      markdownRemark(frontmatter: {id: {eq: "index-page"}}) {
+        frontmatter {
+          qa {
+            question
+            answer
+          }
+        }
+      }
+    }
+  `);
+
+  return data.markdownRemark.frontmatter.qa;
+};
 
 const QuestionsWrapper = styled('section')(({ theme }) => ({
   display: 'flex',
@@ -29,40 +45,44 @@ const QuestionsInner = styled(Container)(() => ({
   alignItems: 'center',
 }));
 
-const Questions = () => (
-  <QuestionsWrapper>
-    <Typography
-      sx={{
-        mb: {
-          xs: 5,
-          md: 8,
-        },
-      }}
-      variant="h2"
-      component="h2"
-    >
-      Вопросы
-    </Typography>
-    <QuestionsInner>
-      {questionsMock.map(({ question, answer }) => (
-        <Accordion sx={{ width: '100%' }} key={question}>
-          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography
-              variant="h6"
-              component="div"
-            >
-              {question}
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              {answer}
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-      ))}
-    </QuestionsInner>
-  </QuestionsWrapper>
-);
+const Questions = () => {
+  const questions = useData();
+
+  return (
+    <QuestionsWrapper>
+      <Typography
+        sx={{
+          mb: {
+            xs: 5,
+            md: 8,
+          },
+        }}
+        variant="h2"
+        component="h2"
+      >
+        Вопросы
+      </Typography>
+      <QuestionsInner>
+        {questions.map(({ question, answer }) => (
+          <Accordion sx={{ width: '100%' }} key={question}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography
+                variant="h6"
+                component="div"
+              >
+                {question}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                {answer}
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </QuestionsInner>
+    </QuestionsWrapper>
+  );
+};
 
 export default Questions;
