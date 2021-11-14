86238d35581302f62b89bee92bd12d9ee54277be
Ravil <tuychin@gmail.com>
2021-11-15 02:43:46 +0300

    feat(target_banner): get content from graphql

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 import Box from '@mui/material/Box';
-
 import Showcase from '../sections/Showcase';
 import Information from '../sections/Information';
 import Benefits from '../sections/Benefits';
@@ -8,18 +8,38 @@ import Coach from '../sections/Coach';
 import Questions from '../sections/Questions';
 import TargetBanner from '../components/TargetBanner';
 
-const MainPage = () => (
-  <Box>
-    <Showcase />
-    <Information />
-    <Benefits />
-    <Coach />
-    <Questions />
-    <TargetBanner
-      text="Что смотришь? Записывайся на первую тренировку, она бесплатная."
-      buttonText="Как записаться?"
-    />
-  </Box>
-);
+const useData = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      markdownRemark(frontmatter: {id: {eq: "index-page"}}) {
+        frontmatter {
+          target_banner {
+            text
+            button
+          }
+        }
+      }
+    }
+  `);
+
+  return data.markdownRemark.frontmatter.target_banner;
+};
+
+const MainPage = () => {
+  const { text, button } = useData();
+  return (
+    <Box>
+      <Showcase />
+      <Information />
+      <Benefits />
+      <Coach />
+      <Questions />
+      <TargetBanner
+        text={text}
+        buttonText={button}
+      />
+    </Box>
+  );
+};
 
 export default MainPage;
