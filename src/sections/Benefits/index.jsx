c37e21463fe8fd529ae4dbd949ff9144319ee785
Ravil <tuychin@gmail.com>
2021-11-15 01:35:49 +0300

    feat(Benefits): get content from graphql

--- src/sections/Benefits/index.jsx
+++ src/sections/Benefits/index.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 import { styled } from '@mui/material/styles';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
-import benefitsMock from './mock-data';
 import BenefitItem from '../../components/BenefitItem';
+
+const useData = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      markdownRemark(frontmatter: {id: {eq: "index-page"}}) {
+        frontmatter {
+          benefits {
+            title
+            description
+            image
+            tags {
+              tag
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  return data.markdownRemark.frontmatter.benefits;
+};
 
 const BenefitsWrapper = styled('section')(({ theme }) => ({
   display: 'flex',
@@ -25,37 +46,45 @@ const BenefitsInner = styled(Container)(() => ({
   flexWrap: 'wrap',
 }));
 
-const Benefits = () => (
-  <BenefitsWrapper>
-    <Typography
-      sx={{
-        mb: {
-          xs: 5,
-          md: 8,
-        },
-      }}
-      variant="h2"
-      component="h2"
-    >
-      Преимущества
-    </Typography>
-    <BenefitsInner>
-      {benefitsMock.map(({
-        img,
-        title,
-        description,
-        tags,
-      }) => (
-        <BenefitItem
-          img={img}
-          title={title}
-          description={description}
-          tags={tags}
-          key={title}
-        />
-      ))}
-    </BenefitsInner>
-  </BenefitsWrapper>
-);
+const Benefits = () => {
+  const benefits = useData();
+
+  return (
+    <BenefitsWrapper>
+      <Typography
+        sx={{
+          mb: {
+            xs: 5,
+            md: 8,
+          },
+        }}
+        variant="h2"
+        component="h2"
+      >
+        Преимущества
+      </Typography>
+      <BenefitsInner>
+        {benefits.map(({
+          title,
+          description,
+          image,
+          tags,
+        }) => {
+          const transformedTags = tags?.map(({ tag }) => tag) || [];
+
+          return (
+            <BenefitItem
+              img={image}
+              title={title}
+              description={description}
+              tags={transformedTags}
+              key={title}
+            />
+          );
+        })}
+      </BenefitsInner>
+    </BenefitsWrapper>
+  );
+};
 
 export default Benefits;
