a58b3c0cf3f1c85c38abced2e3d5582c89e94fc3
Ravil <tuychin@gmail.com>
2021-11-17 02:40:24 +0300

    refactor(project): const

--- src/const.js
+++ src/const.js
@@ -3,30 +3,6 @@ export const pages = [
   { name: 'О нас', href: '/about' },
 ];
 
-export const Location = {
-  ADDRESS: 'г. Москва, ул. Ленинская Слобода, 19 (Т.Ц. Омега Плаза)',
-  COORDINATES: [55.708303, 37.652822],
-};
-
-export const TelNumber = {
-  LABEL: '+7 (925) 635 54 55',
-  HREF: 'tel:+79256355455',
-};
-
-export const Telegram = {
-  LABEL: '@tuychin_r',
-  HREF: 'https://t.me/tuychin_r',
-};
-
-export const Instagram = {
-  LABEL: 'tuychin.r',
-  HREF: 'https://www.instagram.com/tuychin.r',
-};
-
 export default {
   pages,
-  Location,
-  TelNumber,
-  Telegram,
-  Instagram,
 };
